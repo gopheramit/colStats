4c5879af2127aa16d01635e5de70d89fa1676bfd
gopheramit <amitwpu@gmail.com>
2023-03-10 19:34:19 +0530

    imporoving performance

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"runtime"
 	"sync"
 )
 
@@ -40,28 +41,38 @@ func run(filenames []string, op string, column int, out io.Writer) error {
 	resCh := make(chan []float64)
 	errCh := make(chan error)
 	doneCh := make(chan struct{})
+	filesCh := make(chan string)
 	wg := sync.WaitGroup{}
-	for _, fname := range filenames {
+	go func() {
+		defer close(filesCh)
+		for _, fname := range filenames {
+			filesCh <- fname
+		}
+	}()
+	for i := 0; i < runtime.NumCPU(); i++ {
 		wg.Add(1)
-		go func(fname string) {
+		go func() {
 			defer wg.Done()
+			for fname := range filesCh {
+				f, err := os.Open(fname)
+				if err != nil {
+					//return fmt.Errorf("cannot open file :%w", err)
+					errCh <- fmt.Errorf("Cannot open file%w", err)
+					return
+				}
+				data, err := csv2float(f, column)
+				if err != nil {
+					errCh <- err
 
-			f, err := os.Open(fname)
-			if err != nil {
-				//return fmt.Errorf("cannot open file :%w", err)
-				errCh <- fmt.Errorf("Cannot open file%w", err)
-				return
-			}
-			data, err := csv2float(f, column)
-			if err != nil {
-				errCh <- err
+				}
+				if err := f.Close(); err != nil {
+					errCh <- err
+				}
+				resCh <- data
 
 			}
-			if err := f.Close(); err != nil {
-				errCh <- err
-			}
-			resCh <- data
-		}(fname)
+
+		}()
 	}
 
 	go func() {
